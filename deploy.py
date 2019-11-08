8d78a8eff70b5e972c4479e567fd5b00a5252e64
Kiharu Shishikura <mutobow0414@yahoo.co.jp>
2019-11-08 10:50:24 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,14 +1,13 @@
-import pyxie
-from pyxie import devtool
+import igeCore as core
+from igeCore import devtool
 
 devtool.deploy('tutorial01', 'tutorial', 'tutorial01', '10')
-#devtool.deploy('tutorial02', 'tutorial', 'tutorial02', '10')
-#devtool.deploy('tutorial03', 'tutorial', 'tutorial03', '10')
-#devtool.deploy('tutorial04', 'tutorial', 'tutorial04', '10')
-#devtool.deploy('tutorial05', 'tutorial', 'tutorial05', '10')
+devtool.deploy('tutorial02', 'tutorial', 'tutorial02', '10')
+devtool.deploy('tutorial03', 'tutorial', 'tutorial03', '10')
+devtool.deploy('tutorial04', 'tutorial', 'tutorial04', '10')
+devtool.deploy('tutorial05', 'tutorial', 'tutorial05', '10')
 
-
-#import Tutorial06.deploy
-#Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', pyxie.TARGET_PLATFORM_PC)
-#Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', pyxie.TARGET_PLATFORM_IOS)
-#Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', pyxie.TARGET_PLATFORM_ANDROID)
+import Tutorial06.deploy
+Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', core.TARGET_PLATFORM_PC)
+Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', core.TARGET_PLATFORM_IOS)
+Tutorial06.deploy.deployPlatform('Tutorial06', 'Castle_200', 'tutorial06', 'tutorial', '10', core.TARGET_PLATFORM_ANDROID)
