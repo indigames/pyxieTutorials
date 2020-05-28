cea5410ce7736d738a0c574a74567a2423f1bc39
Kiharu Shishikura <mutobow0414@yahoo.co.jp>
2020-05-28 15:46:34 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,18 +1,2 @@
 from igeCore import devtool
-
-# increase this before deploy
-version = '1'
-
-devtool.deploy('box2D', 'igeTutorial', 'Box2D', version)
-devtool.deploy('bullet', 'igeTutorial', 'Bullet', version)
-devtool.deploy('bulletBox', 'igeTutorial', 'BulletBox', version)
-devtool.deploy('character3D', 'igeTutorial', 'character3D', version)
-devtool.deploy('firebase', 'igeTutorial', 'firebase', version)
-devtool.deploy('imgui', 'igeTutorial', 'imgui', version)
-devtool.deploy('liquidfun', 'igeTutorial', 'liquidfun', version)
-devtool.deploy('nanoGUI', 'igeTutorial', 'nanoGUI', version)
-devtool.deploy('openCV', 'igeTutorial', 'openCV', version)
-devtool.deploy('profiler', 'igeTutorial', 'profiler', version)
-devtool.deploy('sprite', 'igeTutorial', 'sprite', version)
-devtool.deploy('text', 'igeTutorial', 'text', version)
-
+devtool.deploy('haptic', 'tutorial', 'haptic', '10')
