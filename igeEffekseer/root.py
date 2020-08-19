b618d0112690ed614cf6d6f4c28dd65a31101a13
Do Minh Doan <doan.do@indigames.net>
2020-08-19 15:42:14 +0700

    updated

--- igeEffekseer/root.py
+++ igeEffekseer/root.py
@@ -10,10 +10,8 @@ from igeCore.apputil.imguirenderer import ImgiIGERenderer
 
 # open or resize window (This function is valid only on PC,Ignored in smartphone apps)
 core.window(True, 480, 640)
-# core.window(True, 1280, 720)
 
 imgui.create_context()
-
 impl = ImgiIGERenderer()
 
 import igeEffekseer
@@ -30,30 +28,31 @@ _w, _h = core.deviceSize()
 igeEffekseer.texture_loader(f_texture_loader)
 _particle = igeEffekseer.particle()
 
-# _particle.camera_eye = (10.0, 5.0, -20.0)
-_particle.camera_eye = (0.0, 0.0, 20.0)
-_particle.camera_at = (0.0, 0.0, 0.0)
-_particle.camera_up = (0.0, 1.0, 0.0)
-
-_particle.projection_ortho = False
-_particle.projection_viewsize = (_w / 10, _h / 10)
-
-# _particle.projection_viewport = (_w, _h)
-_particle.projection_near = 1.0
-_particle.projection_far = 1000.0
-_particle.projection_fov = 90.0
-
 _particle.framerate = 60.0
+_handle = _particle.add('Effects/MagicTornade.efk')
+_particle.set_scale(_handle, 0.25, 0.25, 0.25)
+
+_rotation = 0.0, 0.0, 0.0
+_position = 0.0, 0.0, 0.0
 
 _distortion_hd = -1
 _laser_hd = -1
 _homing_hd = -1
-_model_hd = -1
-
 _tornade_hd = -1
-_rotation_value = 0.0
 
 _color = 1.0, 0.0, 1.0, 1.0
+
+showcase = core.showcase("showcase")
+camera = core.camera("cam01")
+# camera.orthographicProjection = True
+
+__w, __h = core.viewSize()
+
+camera.orthoHeight = __h / 50
+camera.position = (0, 0, 10)
+
+figure = core.figure('Sapphiart/Sapphiart')
+showcase.add(figure)
 
 while True:
     w, h = core.viewSize()
@@ -67,99 +66,24 @@ while True:
         press = touch['is_holded'] | touch['is_moved']
     impl.process_inputs()
 
-    core.update()       
-
-    imgui.new_frame()
+    core.update()
     
+    camera.shoot(showcase)
+    if _particle is not None:
+        _particle.shoot(camera.projectionMatrix, vmath.inverse(camera.viewInverseMatrix), core.getElapsedTime())
+    
+    imgui.new_frame()    
     imgui.begin("Effekseer", True)
     
-    imgui.push_id('Particle')
-    imgui.text('Particle')
-    imgui.same_line()
-    if imgui.button("Init") and _particle is None:
-        _particle = igeEffekseer.particle()
-    imgui.same_line()
-    if imgui.button("Destroy"):
-        del _particle
-        _particle = None
-    imgui.pop_id()
-    
-    if _particle is not None:
-        imgui.text('drawcall:' + str(_particle.drawcall_count()) + ' vertex:' + str(_particle.vertex_count()))
-        imgui.text('update:' + str(_particle.update_time()) + ' draw:' + str(_particle.draw_time()))
-        
-        imgui.push_id('Simple_Distortion')
-        imgui.text('Distortion')
-        imgui.same_line()
-        if imgui.button("play"):
-            _distortion_hd = _particle.add('Effects/Simple_Distortion.efk')
-        imgui.same_line()    
-        if imgui.button("move"):
-            _particle.set_location(_distortion_hd, 0.0, -10.0, 0.0) 
-        imgui.same_line()
-        if imgui.button("pause?"):
-            _particle.set_pause(_distortion_hd, not _particle.get_pause(_distortion_hd))
-        imgui.same_line()
-        if imgui.button("show?"):
-            _particle.set_shown(_distortion_hd, not _particle.get_shown(_distortion_hd))
-        imgui.same_line()    
-        if imgui.button("stop"):
-            _particle.remove(_distortion_hd)            
-        _, _color = imgui.color_edit4("color", *_color, show_alpha=True)
-        _particle.set_color(_distortion_hd, int(_color[0] * 255), int(_color[1] * 255), int(_color[2] * 255), int(_color[3] * 255))
-        imgui.pop_id()
-        
-        imgui.push_id('Laser01')
-        imgui.text('Laser01')
-        imgui.same_line()
-        if imgui.button("play"):
-            _laser_hd = _particle.add('Effects/Laser01.efk', position=(0.0, 0.0, 0.0))
-        imgui.same_line()    
-        if imgui.button("stop"):
-            _particle.remove(_laser_hd)          
-        imgui.pop_id()
-        
-        imgui.push_id('Model')
-        imgui.text('Model')
-        imgui.same_line()
-        if imgui.button("play"):
-            _model_hd = _particle.add('Effects/Simple_Model_UV.efk', position=(0.0, -10.0, 0.0))
-        imgui.same_line()    
-        if imgui.button("stop"):
-            _particle.remove(_model_hd)          
-        imgui.pop_id()
-        
-        imgui.push_id('Homing_Laser01')
-        imgui.text('Homing_Laser01')
-        imgui.same_line()
-        if imgui.button("play"):
-            _homing_hd = _particle.add('Effects/Homing_Laser01.efk', position=(0.0, -10.0, 0.0))
-        imgui.same_line()    
-        if imgui.button("stop"):
-            _particle.remove(_homing_hd)
-        imgui.pop_id()
-        
-        imgui.push_id('MagicTornade')
-        imgui.text('MagicTornade')
-        imgui.same_line()
-        if imgui.button("play"):
-            _tornade_hd = _particle.add('Effects/MagicTornade.efk', position=(0.0, -10.0, 0.0))
-        imgui.same_line()
-        if imgui.button("stop"):
-            _particle.remove(_tornade_hd)
-        imgui.pop_id()
-        changed, _rotation_value = imgui.slider_float("Rotation", _rotation_value, min_value=0.0, max_value=3.14 * 2,format="%.2f", power=1)
-        if changed is True:
-            _particle.set_rotation(_tornade_hd, _rotation_value, 0.0, 0.0)
-        
-        if imgui.button("Stop All"):
-            _particle.stop_all_effects()  
+    changed, _rotation = imgui.slider_float3("Rotation", *_rotation, min_value=0.0, max_value=3.14 * 2,format="%.2f", power=1)
+    if changed is True:
+        _particle.set_rotation(_handle, _rotation[0], _rotation[1], _rotation[2])    
+
+    position_changed, _position = imgui.slider_float3("Position", *_position, min_value=-10.0, max_value=10.0,format="%.0f", power=1)
+    if position_changed is True:
+        _particle.set_location(_handle, _position[0], _position[1], _position[2])   
     
     imgui.end()    
     imgui.render()
-    impl.render(imgui.get_draw_data())
-    
-    if _particle is not None:
-        _particle.shoot(core.getElapsedTime())
-    
+    impl.render(imgui.get_draw_data(), clearColor=False)
     core.swap()
