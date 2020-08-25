60184cf21e876cc7104e058e4367ff9c18b907c9
Do Minh Doan <doan.do@indigames.net>
2020-08-25 17:30:45 +0700

    sample

--- igeEffekseer/convert.py
+++ igeEffekseer/convert.py
@@ -1,8 +1,16 @@
 
 import igeCore
 from igeCore import devtool
+import os
 
 # devtool.convertAssets('.','.', igeCore.TARGET_PLATFORM_PC)
 devtool.convertAssets('.','.', igeCore.TARGET_PLATFORM_MOBILE)
 
-
+for root, dirs, files in os.walk("Effects"):
+    for file in files:
+        if file.endswith(".efkproj"):
+            file_input = os.path.join(root, file)
+            file_output = os.path.splitext(file_input)[0] + ".efk"
+            command = "Tool\Effekseer.exe -cui -in " + str(file_input) + " -e " + str(file_output)
+            print('processing ' + file_input)
+            os.system(command)
