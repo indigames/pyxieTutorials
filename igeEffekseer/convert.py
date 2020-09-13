14f33262bda766c467593fc1e9f93e5b6e392eb3
doandoindi <doan.do@indigames.net>
2020-09-13 15:13:01 +0700

    + *.efkefc

--- igeEffekseer/convert.py
+++ igeEffekseer/convert.py
@@ -8,7 +8,7 @@ devtool.convertAssets('.','.', igeCore.TARGET_PLATFORM_MOBILE)
 
 for root, dirs, files in os.walk("Effects"):
     for file in files:
-        if file.endswith(".efkproj"):
+        if file.endswith(".efkproj") or file.endswith(".efkefc"):
             file_input = os.path.join(root, file)
             file_output = os.path.splitext(file_input)[0] + ".efk"
             command = "Tool\Effekseer.exe -cui -in " + str(file_input) + " -e " + str(file_output)
